e0815a9a7000bd1623ad0973cab545a09494bcd1
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-11-17 11:57:40 +0300

    get and set added

--- src/main/java/ru/netology/crud/domain/Issue.java
+++ src/main/java/ru/netology/crud/domain/Issue.java
@@ -19,4 +19,69 @@ public class Issue {
     private String projects;
     private String milestones;
     private Set<String> assignee;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<String> getLabel() {
+        return label;
+    }
+
+    public void setLabel(Set<String> label) {
+        this.label = label;
+    }
+
+    public String getProjects() {
+        return projects;
+    }
+
+    public void setProjects(String projects) {
+        this.projects = projects;
+    }
+
+    public String getMilestones() {
+        return milestones;
+    }
+
+    public void setMilestones(String milestones) {
+        this.milestones = milestones;
+    }
+
+    public Set<String> getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Set<String> assignee) {
+        this.assignee = assignee;
+    }
 }
